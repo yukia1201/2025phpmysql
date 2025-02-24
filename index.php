d9c58a7c4b8710294248d05aeb63a90a0ebfcd6c
yukia1201 <1112211014@gap.wzu.edu.tw>
2025-02-24 14:50:49 +0800

    update

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP & MySOL</title>
+</head>
+<body>
+    <H1>Home Sweet Home</H1>
+    <hr>
+    <p>計算結果: <?= 333*555 ?></p>
+    <hr>
+
+    <p>今天的日期是: <?= Date("Y/m/d") ?></p>
+</body>
+</html>

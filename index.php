5f6592e3c21d8a770917459f2e85fef3b2cd9490
yukia1201 <1112211014@gap.wzu.edu.tw>
2025-03-10 14:06:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>報名表</title>
+    <title>活動報名表</title>
 </head>
 <body>
-    
+    <h1>活動報名表</h1>
 </body>
 </html>

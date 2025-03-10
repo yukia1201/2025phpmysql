38f34ca2f8e675d5014b70fd883a9aa405130e40
yukia1201 <1112211014@gap.wzu.edu.tw>
2025-03-10 16:07:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,39 +13,69 @@
         <legend>基本資料</legend>
         <p>
             <label for="name">姓名</label>
-            <input type="text" name="name" id="name" value="" placeholder="請輸入姓名">
+            <input type="text" name="name" id="name" value="" placeholder="請輸入姓名" required>
         </p>
         <p>
-            <label for="gender">性別</label>
-            <input type="text" name="gender" id="gender" value=" ">
+            <label for="">性別</label>
+            <input type="radio" name="gender" id="gender1" value="1">
+            <label for="gender1">男生</label>
+            <input type="radio" name="gender" id="gender2" value="2">
+            <label for="gender2">女生</label>
         </p>
         <p>
             <label for="birthday">生日</label>
-            <input type="text" name="birthday" id="birthday" value=" ">
+            <input type="date" name="birthday" id="birthday" value="<?= date("Y-m-d") ?>">
         </p>
         <p>
             <label for="telephone">電話</label>
             <input type="text" name="telephone" id="telephone" value=" ">
         </p>
         <p>
-            <label for="area">地區</label>
-            <input type="text" name="area" id="area" value=" ">
-        </p>
-        <p>
-            <label for="address">地址</label>
-            <input type="text" name="address" id="address" value=" ">
+            <label for="area">居住區域</label>
+            <select name="area" id="area">
+                <option value="0">請選擇...</option>
+                <option value="1">北部</option>
+                <option value="2">中部</option>
+                <option value="3">南部</option>
+                <option value="4">東部</option>
+                <option value="5">外島</option>
+            </select>
+                    
         </p>
     </fieldset>
 
     <fieldset>  
         <legend>使用行為</legend>
+        <input type="checkbox" name="behavior[]" id="behavior1">
+        <label for="">聊天</label>
+        <input type="checkbox" name="behavior[]" id="behavior2">
+        <label for="">直播</label>
+        <input type="checkbox" name="behavior[]" id="behavior3">
+        <label for="">書信</label>
+        <input type="checkbox" name="behavior[]" id="behavior4">
+        <label for="">社群</label>
+        <input type="checkbox" name="behavior[]" id="behavior5">
+        <label for="">購物</label>
+        <input type="checkbox" name="behavior[]" id="behavior6">
+        <label for="">金融</label>
     </fieldset>
     <fieldset>  
         <legend>滿意度</legend>
+        <label for="">場地</label>
+            <input type="radio" name="place" id="place1" value="5">
+            <label for="place1">非常滿意</label>
+            <input type="radio" name="place" id="place2" value="4">
+            <label for="place2">滿意</label>
+            <input type="radio" name="place" id="place3" value="3">
+            <label for="place3">普通</label>
+            <input type="radio" name="place" id="place4" value="2">
+            <label for="place4">不滿意</label>
+            <input type="radio" name="place" id="place5" value="1">
+            <label for="place5">非常不滿意</label>
     </fieldset>
     <fieldset>  
         <legend>資料上傳</legend>
     </fieldset>
-
+    <p><input type="submit" value="送出"></p>
 </body>
 </html>

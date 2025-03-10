b012bfaaafa6db70635d619a7fcffc4a75ccb21c
yukia1201 <1112211014@gap.wzu.edu.tw>
2025-03-10 14:44:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,45 @@
 </head>
 <body>
     <h1>活動報名表</h1>
+    <form action="" >
+    
+    <fieldset>  
+        <legend>基本資料</legend>
+        <p>
+            <label for="name">姓名</label>
+            <input type="text" name="name" id="name" value="" placeholder="請輸入姓名">
+        </p>
+        <p>
+            <label for="gender">性別</label>
+            <input type="text" name="gender" id="gender" value=" ">
+        </p>
+        <p>
+            <label for="birthday">生日</label>
+            <input type="text" name="birthday" id="birthday" value=" ">
+        </p>
+        <p>
+            <label for="telephone">電話</label>
+            <input type="text" name="telephone" id="telephone" value=" ">
+        </p>
+        <p>
+            <label for="area">地區</label>
+            <input type="text" name="area" id="area" value=" ">
+        </p>
+        <p>
+            <label for="address">地址</label>
+            <input type="text" name="address" id="address" value=" ">
+        </p>
+    </fieldset>
+
+    <fieldset>  
+        <legend>使用行為</legend>
+    </fieldset>
+    <fieldset>  
+        <legend>滿意度</legend>
+    </fieldset>
+    <fieldset>  
+        <legend>資料上傳</legend>
+    </fieldset>
+
 </body>
 </html>
